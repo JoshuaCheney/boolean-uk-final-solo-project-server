cf915479faa1acbe6eaf09b7315f440e3131bc0f
Josh Cheney <joshcheney125@gmail.com>
2021-12-02 16:52:19 +0000

    updated product controller

--- src/resources/product/controller.js
+++ src/resources/product/controller.js
@@ -3,7 +3,7 @@ const prisma = require("../../utils/database")
 const getAllProducts = async (req, res) => {
     try {
         const result = await prisma.product.findMany()
-        res.json([data = result])
+        res.json(result)
     } catch (error) {
         console.error({ error: error.message })
         res.status(500).json({ error: error.message })
